35595fcf2b76d8a7db0a9d9fb37f5361f731d354
edulx <edulxoverseas@gmail.com>
2025-03-31 14:06:16 +0200

    Update gsheet.py

--- utils/gsheet.py
+++ utils/gsheet.py
@@ -1,15 +1,11 @@
-
 import gspread
 import pandas as pd
 import streamlit as st
 from google.oauth2.service_account import Credentials
 
-# Scopes and credentials
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
-creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+# Use credentials from Streamlit secrets
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+CREDS = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=SCOPES)
 client = gspread.authorize(CREDS)
 
 def get_worksheet_df(sheet_url, tab_name):
@@ -23,10 +19,9 @@ def update_cell_by_value(sheet_url, tab_name, document_name, updates: dict):
     worksheet = sheet.worksheet(tab_name)
     data = worksheet.get_all_records()
 
-    # Find row index where Document Name matches
     for i, row in enumerate(data):
         if row["Document Name"] == document_name:
             for key, value in updates.items():
                 col_index = list(row.keys()).index(key) + 1
-                worksheet.update_cell(i + 2, col_index, value)  # +2 because rows start at 1 and row[0] is headers
+                worksheet.update_cell(i + 2, col_index, value)
             break
